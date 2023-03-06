ec8c9db40dea2b7ac12459a05c68a2f48076ae00
Robert Gallas <gallas.robert@gmail.com>
2023-03-06 17:38:19 +0100

    REst COntroller fix

--- src/main/java/sk/umb/example/library/address/controller/AddressController.java
+++ src/main/java/sk/umb/example/library/address/controller/AddressController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import sk.umb.example.library.address.service.AddressDetailDto;
 import sk.umb.example.library.address.service.AddressRequestDto;
 import sk.umb.example.library.address.service.AddressService;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AddressController {
     private final AddressService addressService;
 
