f4512a888cd66f6bc5b4825816c99d68f7343490
Robert Gallas <gallas.robert@gmail.com>
2023-03-06 17:39:43 +0100

    Fix

--- src/main/java/sk/umb/example/library/address/controller/AddressController.java
+++ src/main/java/sk/umb/example/library/address/controller/AddressController.java
@@ -1,6 +1,5 @@
 package sk.umb.example.library.address.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
