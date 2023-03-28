0f9b388b32ae6c0f295a7201af58e9c6e63cb8dd
jstrmen <jakub.strmen@coreteq.eu>
2023-03-28 08:44:31 +0200

    added - minimalized CORS configuration

--- src/main/java/sk/umb/example/library/authentication/core/WebSecurityConfig.java
+++ src/main/java/sk/umb/example/library/authentication/core/WebSecurityConfig.java
@@ -1,26 +1,19 @@
 package sk.umb.example.library.authentication.core;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sk.umb.example.library.authentication.service.AuthenticationService;
 
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     private final DemoAuthenticationEntryPoint demoAuthenticationEntryPoint;
@@ -36,8 +29,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .cors().configurationSource(corsConfigurationSource())
-                .and()
+                .cors(withDefaults())
                 .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
                 .exceptionHandling().authenticationEntryPoint(demoAuthenticationEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -54,19 +46,6 @@ public class WebSecurityConfig {
                                  "/v3/api-docs/swagger-config",
                                  "/v3/api-docs",
                                  "/swagger-resources/**");
-    }
-
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        corsConfiguration.setAllowedHeaders(List.of("authorization", "content-type"));
-        corsConfiguration.addExposedHeader("Authorization");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
     }
 }
 
